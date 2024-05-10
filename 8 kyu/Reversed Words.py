83f859d5b49490a2382690675ab95e5b7b20d30d
Ochowicz <114605042+Ochowicz@users.noreply.github.com>
2024-05-10 23:14:55 +0200

    Add files via upload

---
+++ 8 kyu/Reversed Words.py
@@ -0,0 +1,5 @@
+# Complete the solution so that it reverses all of the words within the string passed in.
+#
+# Words are separated by exactly one space and there are no leading or trailing spaces.
+def reverse_words(s):
+    return ' '.join(s.split()[::-1])
