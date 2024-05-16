fc545947aa3b8712851376045e5ceb59b1be56e4
Ochowicz <114605042+Ochowicz@users.noreply.github.com>
2024-05-16 22:46:14 +0200

    Add files via upload

---
+++ 8 kyu/Welcome!.py
@@ -0,0 +1,24 @@
+def greet(language):
+    greet = {
+    "english": "Welcome",
+    "czech": "Vitejte",
+    "danish": "Velkomst",
+    "dutch": "Welkom",
+    "estonian": "Tere tulemast",
+    "finnish": "Tervetuloa",
+    "flemish": "Welgekomen",
+    "french": "Bienvenue",
+    "german": "Willkommen",
+    "irish": "Failte",
+    "italian": "Benvenuto",
+    "latvian": "Gaidits",
+    "lithuanian": "Laukiamas",
+    "polish": "Witamy",
+    "spanish": "Bienvenido",
+    "swedish": "Valkommen",
+    "welsh": "Croeso",
+}
+    if language in greet:
+        return greet[language.lower()]
+    else:
+        return "Welcome"
