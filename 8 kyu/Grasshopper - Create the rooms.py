4399a9978a1b5e40a6869d1b4804eca50a7da11f
Ochowicz <114605042+Ochowicz@users.noreply.github.com>
2024-05-22 22:33:58 +0200

    Add files via upload

---
+++ 8 kyu/Grasshopper - Create the rooms.py
@@ -0,0 +1,4 @@
+rooms = {
+    1: {'name':'room1', 'description': 'room1', 'completed': True},
+    2: {'name':'room2', 'description': 'room2', 'completed': True},
+    3: {'name':'room3', 'description': 'room3', 'completed': True}}
